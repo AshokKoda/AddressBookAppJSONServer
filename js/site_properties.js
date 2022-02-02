db1edb63bec0f0cf9df9abcae2a0fae483de3c32
Ashok Koda <a.koda59@gmail.com>
2022-02-02 12:32:04 +0530

    Added:Welcome message display

--- js/site_properties.js
+++
@@ -1,6 +0,0 @@
-let site_properties = {
-    homepage : "../pages/Home.html",
-    addform : "../pages/AddPersonForm.html",
-    localstorage:"true",
-    server_url:"http://localhost:3000/EmployeePayrollDB/"
-}
